92ca9f2f715c63c2ae8b0b7660931b457f2bd119
ouch_ouch <2055010244@kientruchanoi.edu.vn>
2024-02-23 16:30:40 +0700

    Completed onboarding, splash screen

---
+++ app/src/main/java/com/example/storyapp/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.storyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.example.storyapp.databinding.ActivityMainBinding
+import com.example.storyapp.onboarding.ViewPagerAdapter
+import com.example.storyapp.onboarding.screens.FirstFragment
+import com.example.storyapp.onboarding.screens.SecondFragment
+import com.example.storyapp.onboarding.screens.ThirdFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var view: ViewPager2
+    private lateinit var adapter: ViewPagerAdapter
+    private lateinit var indicators: LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        view = findViewById(R.id.viewPager)
+        indicators = findViewById(R.id.indicators)
+        val fragmentList = arrayListOf(FirstFragment(), SecondFragment(), ThirdFragment())
+
+        adapter = ViewPagerAdapter(fragmentList, supportFragmentManager, lifecycle)
+        view.adapter = adapter
+        view.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                // hiện thông báo khi chuyển trang
+                Log.d("scrolled", "${position + 1}")
+                val dotList = arrayListOf<ImageView>(binding.dot1, binding.dot2, binding.dot3)
+                for (i in 0..<adapter.itemCount) {
+                    if (i <= position) {
+                        dotList[i].setImageDrawable(
+                            ContextCompat.getDrawable(
+                                applicationContext,
+                                R.drawable.ic_active_dot
+                            )
+                        )
+                    } else {
+                        dotList[i].setImageDrawable(
+                            ContextCompat.getDrawable(
+                                applicationContext,
+                                R.drawable.ic_inactive_dot
+                            )
+                        )
+                    }
+                }
+                if (position == 1) {
+                    binding.btnPrevious.visibility = View.VISIBLE
+                } else if (position == 2) {
+                    binding.btnNext.text ="start"
+                } else {
+                    binding.btnPrevious.visibility = View.GONE
+                }
+
+            }
+        })
+        binding.btnNext.setOnClickListener {
+            if (view.currentItem < adapter.itemCount - 1) {
+                view.currentItem += 1
+            } else {
+                Toast.makeText(this, "This is the last page", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.btnPrevious.setOnClickListener {
+            if (view.currentItem == 1 || view.currentItem == 2) {
+                view.currentItem -= 1
+            } else {
+                Toast.makeText(this, "This is the first page", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
